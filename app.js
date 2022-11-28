e1bc81bb9c5b8a59a65c24a8df1ba0293e1043f8
gary909 <garywhite61@hotmail.com>
2022-11-28 14:05:17 +0100

    code not wokring BAh

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 function isDigit(s) {
-    //your code
+    let myNum = Number(s);
+    if (isNaN(myNum)) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
-console.log(isDigit("s2324")); // false
 console.log(isDigit("-232.4")); // true
+console.log(isDigit("   3   ")); // true
+console.log(isDigit("3")); // true
+
+console.log(isDigit("3-4")); // false
+console.log(isDigit("  3  5")); // false
+console.log(isDigit("3 5")); // false
+console.log(isDigit("zero")); // false
+console.log(isDigit("s2324")); // false
+console.log(isDigit("....")); // false
